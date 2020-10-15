ea81287f5d3a3b6791896f7e8308a3ec241cf34e
jameelfullstackdev <jameel@fdesigns.co.uk>
2020-10-15 18:40:37 +0500

    front page ui

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {StyleSheet, View, Text,Platform, KeyboardAvoidingView, TextInput} from 'react-native'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView style= {styles.container} behavior="padding">
+        <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>  
+        <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text> 
+        <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+
+
+        <TextInput 
+          autoCorrect={false} 
+          placeholder="Search any city" 
+          placeholderTextColor="white" 
+          style={styles.textInput}
+          clearButtonMode="always" 
+        />
+
+    </KeyboardAvoidingView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({ 
+   container: { 
+     flex: 1, 
+     backgroundColor: 'green', 
+     alignItems: 'center', 
+     justifyContent: 'center', 
+     },  
+    textStyle: {  
+      textAlign: 'center',  
+      fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto', 
+      color: 'white'
+     }, 
+    largeText: { 
+        fontSize: 44, 
+    }, 
+    smallText: { 
+      fontSize: 18, 
+    },
+    textInput : {
+      backgroundColor: '#666',
+      color: 'white', 
+       height: 40, 
+       width: 300, 
+       marginTop: 20, 
+       marginHorizontal: 20, 
+       paddingHorizontal: 10, 
+       alignSelf: 'center'
+    }
+})
+
+export default App;
