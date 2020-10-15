29d2b9ee722b658f86d4d31eaacfea199c87e4d9
jameelfullstackdev <jameel@fdesigns.co.uk>
2020-10-15 19:21:30 +0500

    added background image

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import React from 'react'
-import {StyleSheet, Text,Platform, KeyboardAvoidingView, TextInput} from 'react-native'
+import {StyleSheet, Text,Platform, KeyboardAvoidingView, View, ImageBackground} from 'react-native'
+
+import getImageForWeather from './utils/getImageForWeather';
 
 import SearchInput from './components/SearchInput';
 
 const App = () => {
   return (
     <KeyboardAvoidingView style= {styles.container} behavior="padding">
+      <ImageBackground 
+        source={getImageForWeather('Clear')} 
+        style={styles.imageContainer} 
+        imageStyle={styles.image} >
+
+      <View style={styles.detailsContainer}> 
         <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>  
         <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text> 
         <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
         
         <SearchInput  placeholder="Search any city"  />
+      </View>
+
+       </ImageBackground>
 
      </KeyboardAvoidingView>
   );
@@ -19,20 +30,35 @@ const App = () => {
 const styles = StyleSheet.create({ 
    container: { 
      flex: 1, 
-     backgroundColor: 'white', 
-     alignItems: 'center', 
-     justifyContent: 'center', 
+     backgroundColor: '#34495E', 
      },  
+     detailsContainer: { 
+      flex: 1, 
+      justifyContent: 'center',
+      backgroundColor: 'rgba(0,0,0,0.2)', 
+      paddingHorizontal: 20 
+    },    
     textStyle: {  
       textAlign: 'center',  
       fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto', 
+      color: 'white'
      }, 
     largeText: { 
         fontSize: 44, 
     }, 
     smallText: { 
       fontSize: 18, 
-    }
+    },
+    imageContainer: 
+    { 
+      flex: 1 
+    }, 
+    image: { 
+      flex: 1, 
+      width: null, 
+      height: null, 
+      resizeMode: 'cover'
+    } 
 })
 
 export default App;
